909c4e439c209627637f439787e59eb17c9b0988
Shresht Srivastav <59516096+Shresht7@users.noreply.github.com>
2022-10-29 16:43:04 +0530

    Create Symlinks Script

---
+++ Scripts/symlink.ps1
@@ -0,0 +1,44 @@
+# ========
+# SYMLINKS
+# ========
+
+<#
+.SYNOPSIS
+Shorthand to create a symlink
+.DESCRIPTION
+Creates a symlink at the given path to the given target
+.PARAMETER Path
+Original path to create the symlink
+.PARAMETER Target
+Path to the targetted location
+.EXAMPLE
+Set-Symlink "$HOME\.gitconfig" "$HOME\Configs\Git\.gitconfig"   # Links $HOME\.gitconfig to $HOME\Configs\Git\.gitconfig
+#>
+function Set-Symlink($Path, $Target) {
+    if (Test-Path $Path) { Remove-Item $Path }
+    New-Item -ItemType SymbolicLink -Path $Path -Target $Target
+}
+
+# ---
+# Git
+# ---
+
+Set-Symlink -Path "$HOME\.gitconfig" -Target "$HOME\Configs\Git\.gitconfig"
+
+# ---------- 
+# PowerShell
+# ----------
+
+Set-Symlink -Path "$HOME\Documents\PowerShell\Microsoft.PowerShell_profile.ps1" -Target "$HOME\Configs\PowerShell\Microsoft.PowerShell_profile.ps1"
+
+# ------
+# VSCode
+# ------
+
+Set-Symlink -Path "$HOME\AppData\Roaming\Code\User\settings.json" -Target "$HOME\Configs\VSCode\settings.json"
+
+# ----------------
+# Windows Terminal
+# ----------------
+
+Set-Symlink -Path "$HOME\AppData\Local\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState\settings.json" -Target "$HOME\Configs\Windows-Terminal\settings.json"
