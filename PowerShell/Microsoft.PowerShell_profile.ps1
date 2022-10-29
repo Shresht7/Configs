c9f6cbc3d213044022481783d74da756a60f7a4e
Shresht Srivastav <59516096+Shresht7@users.noreply.github.com>
2022-10-29 14:52:18 +0530

    Create `Search-FullText` PowerShell Utility Function

--- PowerShell/Microsoft.PowerShell_profile.ps1
+++ PowerShell/Microsoft.PowerShell_profile.ps1
@@ -71,6 +71,24 @@ function Show-Previews() {
 }
 Set-Alias preview Show-Previews
 
+# -------
+# RipGrep
+# -------
+
+<#
+.SYNOPSIS
+Performs full-text search
+.DESCRIPTION
+Uses ripgrep (rg) and fuzzy-finder (fzf) to perform an interactive full-text search and shows the preview using bat
+.PARAMETER text
+The text or regular expression to search for
+.EXAMPLE
+Search-FullText TODO:		# Searches for all file containing TODO:
+#>
+function Search-FullText($text) {
+	rg $text --line-number | fzf --delimiter=":" --preview 'bat --style=numbers --color=always --highlight-line {2} --line-range {2}: {1}'
+}
+
 # ------------------
 # Z Directory Jumper
 # ------------------
