45e81ea5cb7fb9cc1ca539d4ed74c27821e5d4d8
Shresht Srivastav <59516096+Shresht7@users.noreply.github.com>
2022-11-04 21:34:41 +0530

    Move `Search-Web` into utilities

--- PowerShell/Modules/utilities/utilities.psm1
+++ PowerShell/Modules/utilities/utilities.psm1
@@ -44,3 +44,55 @@ function New-Symlink($Path, $Target) {
     New-Item -ItemType SymbolicLink -Path $Path -Target $Target
 }
 Set-Alias symlink New-Symlink
+
+# ---------
+# Find-Path
+# ---------
+
+<#
+.SYNOPSIS
+Find the path of the given program's executable
+.DESCRIPTION
+Locates the path for the given program's executable like the Unix `which` command.
+.PARAMETER command
+Name of the command
+.EXAMPLE
+Find-Path git		# Returns C:\Program Files\Git\cmd\git.exe
+#>
+function Find-Path($command) {
+    Get-Command -Name $command -ErrorAction SilentlyContinue | Select-Object -ExpandProperty Path -ErrorAction SilentlyContinue
+}
+Set-Alias which Find-Path
+
+# ----------
+# Search-Web
+# ----------
+
+<#
+.SYNOPSIS
+Search the web
+.DESCRIPTION
+Launches the default web-browser to do a web-search using a search-engine
+.PARAMETER query
+The search query to perform
+.PARAMETER engine
+The search engine to use to perform the search
+.EXAMPLE
+Search-Web 'PowerShell Documentation'			# Searches the web for 'PowerShell Documentation' using the default search engine
+Search-Web 'Microsoft PowerToys' bing			# Searches the web for 'Microsoft PowerToys' using the 'bing' search engine
+Search-Web -engine github -query 'Terminal'		# Searches GitHub for 'Terminal'
+#>
+function Search-Web($query, $engine = "google") {
+    # Prompt for query if null
+    if ($null -eq $query) {
+        $query = Read-Host "Search Query "
+    }
+    # Load and parse search-engine data  # TODO: Move the searchEngines.json file someplace central so that it can be used by other programs
+    $searchEngines = Get-Content "~\Configs\PowerShell\searchEngines.json" | ConvertFrom-Json | Where-Object { $_.shortcut -ieq $engine }
+    # Encode the query string
+    $encodedQuery = [System.Web.HttpUtility]::UrlEncode($query)
+    # Build the search query URL
+    $search = $searchEngines | ForEach-Object { $_.url.Replace("%s", $encodedQuery) }
+    # Launch the URL using the Start-Process cmdlet (opens the URL with the default browser)
+    Start-Process $search
+}
