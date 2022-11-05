92f5b8760b9ffb33f65a83c7abf112ff6cb9d319
Shresht Srivastav <59516096+Shresht7@users.noreply.github.com>
2022-11-05 13:49:51 +0530

    Create `Remove-PSReadLineHistoryItems` PowerShell Utility cmdlet

--- PowerShell/Microsoft.PowerShell_profile.ps1
+++ PowerShell/Microsoft.PowerShell_profile.ps1
@@ -39,8 +39,22 @@ Set-PSReadLineKeyHandler -Key DownArrow -Function HistorySearchForward
 Set-PSReadLineOption -HistorySearchCursorMovesToEnd
 
 function Get-PSReadLineHistoryPath { (Get-PSReadLineOption).HistorySavePath }
-function Show-PSReadLineHistory { Get-Content (Get-PSReadLineHistoryPath) }
-function Remove-PSReadLineHistory { Remove-Item (Get-PSReadLineHistoryPath) }
+function Get-PSReadLineHistory { Get-Content (Get-PSReadLineHistoryPath) }
+
+function Remove-PSReadLineHistoryItems($MarkedForRemoval = (Get-PSReadLineHistory | Invoke-Fzf -Multi)) {
+	# Get the PSReadLineHistory
+	$ReadlineHistory = Get-PSReadLineHistory
+	
+	# TODO: Add Flag to only remove duplicates (leave 1 entry behind)
+	
+	# Iterate over all items that are marked-for-removal and filter the ReadLineHistory
+	foreach ($filter in $MarkedForRemoval) {
+		$ReadlineHistory = $ReadlineHistory | Where-Object { $_ -cne $filter }
+	}
+
+	# Set Content of the PSReadLineHistory
+	$ReadlineHistory | Set-Content (Get-PSReadLineHistoryPath)
+}
 
 # Prevent annoying beeping noises
 # Set-PSReadLineOption -BellStyle None
