535ad1bb4a90d05d1e1e445b08b1c1d9f8f79a74
Shresht Srivastav <59516096+Shresht7@users.noreply.github.com>
2022-11-13 13:14:38 +0530

    Create `Invoke-Script` Utility Function
    `Invoke-Script` Fuzzy-Finds a script and runs it.

--- PowerShell/Modules/FzfUtils/FzfUtils.psm1
+++ PowerShell/Modules/FzfUtils/FzfUtils.psm1
@@ -62,3 +62,11 @@ Set-Alias fts Search-FullText
 
 Export-ModuleMember -Function Search-FullText
 Export-ModuleMember -Alias fts
+
+function Invoke-Script($ScriptsLocation = "$HOME\Scripts") {
+    . (Get-ChildItem $ScriptsLocation | Invoke-Fzf -Preview "bat --style=numbers --color=always {}")
+}
+Set-Alias script Invoke-Script
+
+Export-ModuleMember -Function Invoke-Script
+Export-ModuleMember -Alias script
