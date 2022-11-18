700a423fc556cb69e45657324ee658a58d137b46
Shresht Srivastav <59516096+Shresht7@users.noreply.github.com>
2022-11-18 14:39:29 +0530

    Create Start-App Utility Function

--- PowerShell/Microsoft.PowerShell_profile.ps1
+++ PowerShell/Microsoft.PowerShell_profile.ps1
@@ -128,3 +128,25 @@ Register-ArgumentCompleter -Native -CommandName winget -ScriptBlock {
         [System.Management.Automation.CompletionResult]::new($_, $_, 'ParameterValue', $_)
     }
 }
+
+<#
+.SYNOPSIS
+Starts an application
+.DESCRIPTION
+Start an application from the shell:AppsFolder
+.PARAMETER AppName
+Name of the Application. (Use `Get-StartApps` to get a list of apps)
+.EXAMPLE
+Start-App Clock
+Start the Windows Clock application
+.EXAMPLE
+Get-StartApps | Invoke-Fzf | Start-App
+Interactively select the app using fzf and start it
+#>
+function Start-App(
+    [Parameter(Mandatory = $True, ValueFromPipeline = $True)]
+    [string]$AppName
+) {
+    $App = Get-StartApps | Where-Object { $_.Name -eq $AppName }
+    Start-Process "explorer.exe" -ArgumentList "shell:AppsFolder\$(($App).AppId)"
+}
