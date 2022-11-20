506c8737c5cc827bdd95902f02e65c13b23e27d7
Shresht Srivastav <59516096+Shresht7@users.noreply.github.com>
2022-11-20 12:06:11 +0530

    Add ShouldProcess support

--- PowerShell/Modules/Backup/Backup.psm1
+++ PowerShell/Modules/Backup/Backup.psm1
@@ -23,41 +23,61 @@ Create a copy of the important-file.txt in the default backup folder
 Backup-Item -Type Archive -Item important-folder -BackupPath "$HOME/NewBackup"
 Creates an archive (.zip) backup of the important-folder in the user defined "$HOME/NewBackup" folder
 #>
-function Backup-Item(
-    # Path of the item to backup
-    [Parameter(
-        Mandatory,
-        ValueFromPipeline,
-        ValueFromPipelineByPropertyName
-    )]
-    [ValidateScript({ Test-Path $Name })]
-    [string]$Name,
+function Backup-Item {
 
-    [ValidateSet("Archive", "Copy")]
-    [string]$Type = "Archive",
+    [CmdletBinding(SupportsShouldProcess = $True, ConfirmImpact = "MEDIUM")]
+    Param (
+        # Path of the item to backup
+        [Parameter(
+            Mandatory,
+            ValueFromPipeline,
+            ValueFromPipelineByPropertyName
+        )]
+        [ValidateScript({ Test-Path (Resolve-Path $_) })]
+        [string]$Name,
 
-    [Parameter()]
-    [string]$BackupPath = $DefaultBackupPath
-) {
+        # The type of backup to create. "Archive" to create a `.zip` file or "Copy" to copy the contents as is
+        [ValidateSet("Archive", "Copy")]
+        [string]$Type = "Archive",
+    
+        # Path to the backup directory
+        [string]$BackupPath = $DefaultBackupPath
+    )
+    
     Begin {
+        # Create the Backup Directory if it doesn't exist
         if (-Not (Test-Path $BackupPath -PathType Container)) {
             New-Item -ItemType Directory $BackupPath
         }
     }
-    Process {
-        $Date = Get-Date -Format FileDateTimeUniversal
-        $Name = (Get-Item $Name).Name
-        $Destination = Join-Path $BackupPath $Name "$Date`_$Name"
     
-        if ($Type -eq "Archive") {
-            Compress-Archive -Path $Name -CompressionLevel Optimal -DestinationPath "$Destination.zip"
-        }
-        if ($Type -eq "Copy") {
-            Copy-Item -Path $Name -Destination $Destination -Recurse
+    Process {
+        # Gather Information
+        $Date = Get-Date -Format FileDateTimeUniversal
+        $Item = Get-Item $Name
+        $Destination = Join-Path $BackupPath $Item.BaseName "$Date`_$($item.Name)"
+    
+        # Create the destination if it doesn't already exist
+        if (-Not (Test-Path $Destination)) {
+            New-Item $Destination -Force
         }
 
+        # Perform Backup Operation
+        if ($PSCmdlet.ShouldProcess($Destination, "Backing up $Name to $Destination")) {
+            switch ($Type) {
+                "Archive" {
+                    Compress-Archive -Path $Name -CompressionLevel Optimal -DestinationPath "$Destination.zip"
+                    break
+                }
+                "Copy" {
+                    Copy-Item -Path $Name -Destination $Destination -Recurse
+                    break
+                }
+            }
+        }
         # TODO: Add logging capability. Write-Output of backup operation so the user can keep a record. Also pipe to file.
     }
+
     End { }
 }
 
