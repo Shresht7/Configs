93284a987e8976d0e8ff840256f700687680f896
Shresht Srivastav <59516096+Shresht7@users.noreply.github.com>
2022-11-25 11:10:56 +0530

    Create `Get-Size` Cmdlet

--- PowerShell/Modules/Utilities/Utilities.psm1
+++ PowerShell/Modules/Utilities/Utilities.psm1
@@ -71,6 +71,55 @@ function Get-BrokenSymlinks(
     Get-ChildItem $Path -Recurse:$Recurse | Where-Object { $null -ne $_.LinkTarget -And -Not (Test-Path -Path $_.LinkTarget) }
 }
 
+<#
+.SYNOPSIS
+Get size of the given path
+.DESCRIPTION
+Measures the size of the given path
+.PARAMETER $Path
+Path to measure the size of
+.PARAMETER $Recurse
+Recursively measure the size of the path
+.EXAMPLE
+Get-Size
+Get the size of the current directory
+Get-Size -Recurse
+Get the size of the current directory by recursing into sub-directories
+#>
+function Get-Size(
+    [Parameter(ValueFromPipeline, ValueFromPipelineByPropertyName)]
+    [ValidateScript({ Test-Path $_ })]
+    [string]$Path = ".",
+
+    [switch]$Recurse
+) {
+    $Item = Get-Item $Path
+
+    # If the Item is a directory
+    if ($Item.PSIsContainer) {
+        $Output = $Item | Get-ChildItem -Recurse:$Recurse | Measure-Object -Sum Length | Select-Object `
+        @{Name = "Path"; Expression = { $Item.FullName } },
+        @{Name = "Files"; Expression = { $_.Count } },
+        @{Name = "Size"; Expression = { $_.Sum } },
+        @{Name = "Bytes"; Expression = { $_.Sum } },
+        @{Name = "Kilobytes"; Expression = { $_.Sum / 1Kb } },
+        @{Name = "Megabytes"; Expression = { $_.Sum / 1Mb } },
+        @{Name = "Gigabytes"; Expression = { $_.Sum / 1Gb } }
+    }
+    # Else if the item is a file
+    else {
+        $Output = $Item | Select-Object `
+        @{Name = "Path"; Expression = { $Item.FullName } },
+        @{Name = "Size"; Expression = { $_.Length } },
+        @{Name = "Bytes"; Expression = { $_.Length } },
+        @{Name = "Kilobytes"; Expression = { $_.Length / 1Kb } },
+        @{Name = "Megabytes"; Expression = { $_.Length / 1Mb } },
+        @{Name = "Gigabytes"; Expression = { $_.Length / 1Gb } }
+    }
+
+    return $Output
+}
+
 # ---------
 # Find-Path
 # ---------
