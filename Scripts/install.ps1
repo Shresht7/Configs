1c68ae6d93fbed23a44b1d75ab8b618683093191
Shresht Srivastav <59516096+Shresht7@users.noreply.github.com>
2022-11-25 23:16:18 +0530

    Refactor Install Script

--- Scripts/install.ps1
+++ Scripts/install.ps1
@@ -2,13 +2,8 @@
 # Installation
 # ------------
 
-# TODO: Verify Elevated Permissions
-
-# Update Help for Modules
-# -----------------------
-
-Write-Host "Updating Help ..."
-Update-Help -Force
+# Exit script if not running as administrator
+if (-Not (Test-IsElevated)) { Write-Error -Message "Requires Administrator Privileges!"; return }
 
 # Install PowerShell Modules
 # --------------------------
@@ -20,3 +15,27 @@ Install-Module PSFzf
 Install-Module z
 
 # TODO: Add Scoop and WinGet packages
+
+# Update Help for Modules
+# -----------------------
+
+Write-Host "Updating Help ..."
+Update-Help -Force
+
+# ----------------
+# Helper Functions
+# ----------------
+
+<#
+.SYNOPSIS
+Checks if the current script has administrator privileges
+.DESCRIPTION
+Checks if the current script is running in administrator mode. Return true if it does, false if it doesn't
+#>
+function Test-IsElevated {
+    # Get the ID and security principal of the current user account
+    $MyIdentity = [System.Security.Principal.WindowsIdentity]::GetCurrent()
+    $MyPrincipal = New-Object System.Security.Principal.WindowsPrincipal($MyIdentity)
+    # Check to see if we are currently running in "Administrator" mode
+    return $MyPrincipal.IsInRole([System.Security.Principal.WindowsBuiltInRole]::Administrator)
+}
