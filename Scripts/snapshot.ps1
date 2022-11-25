b17e9e4809acd4fa08bbd54d9f605ea2bce3bd10
Shresht Srivastav <59516096+Shresht7@users.noreply.github.com>
2022-11-25 20:19:27 +0530

    Snapshot

--- Scripts/snapshot.ps1
+++ Scripts/snapshot.ps1
@@ -7,13 +7,14 @@
 Find the path of the given program's executable
 .DESCRIPTION
 Locates the path for the given program's executable like the Unix `which` command.
-.PARAMETER command
+.PARAMETER $Command
 Name of the command
 .EXAMPLE
-Find-Path git		# Returns C:\Program Files\Git\cmd\git.exe
+Find-Path git
+Returns C:\Program Files\Git\cmd\git.exe
 #>
-function Find-Path($command) {
-    Get-Command -Name $command -ErrorAction SilentlyContinue | Select-Object -ExpandProperty Path -ErrorAction SilentlyContinue
+function Find-Path($Command) {
+    Get-Command -Name $Command -ErrorAction SilentlyContinue | Select-Object -ExpandProperty Path -ErrorAction SilentlyContinue
 }
 
 # ------
