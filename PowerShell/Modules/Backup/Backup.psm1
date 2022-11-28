c431d926c05d4cf41099651c97d20833c6353991
Shresht Srivastav <59516096+Shresht7@users.noreply.github.com>
2022-11-28 09:59:21 +0530

    Add `-Confirm` and `-WhatIf` switches

--- PowerShell/Modules/Backup/Backup.psm1
+++ PowerShell/Modules/Backup/Backup.psm1
@@ -183,11 +183,15 @@ function Remove-OldBackups(
     # TODO: Change this to UInt32 to prevent adding negative days. (Ok while testing)
     [Int32]$Age = 31,
     
-    [String]$BackupPath = $DefaultBackupPath
+    [String]$BackupPath = $DefaultBackupPath,
+
+    [switch] $WhatIf,
+
+    [switch] $Confirm
 ) {
     # Get the list of backups that match the given criteria
     $Backups = if ($null -eq $Name) { Get-ChildItem -Path $BackupPath } else { Get-ChildItem -Path $Backups -Filter $Name }
 
     # Remove backups older than the set age (in days)
-    $Backups | Where-Object { (Get-Date) -lt $_.CreationTime.AddDays(-$days) } | Remove-Item -Confirm
+    $Backups | Where-Object { (Get-Date) -lt $_.CreationTime.AddDays(-$days) } | Remove-Item -Confirm:$Confirm -WhatIf:$WhatIf
 }
