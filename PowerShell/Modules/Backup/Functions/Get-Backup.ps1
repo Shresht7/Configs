6249d5160e617a0224411a666825ff2445baebd0
Shresht Srivastav <59516096+Shresht7@users.noreply.github.com>
2022-11-28 12:57:44 +0530

    Refactor Restore Item

--- PowerShell/Modules/Backup/Functions/Get-Backup.ps1
+++ PowerShell/Modules/Backup/Functions/Get-Backup.ps1
@@ -14,8 +14,17 @@ Returns a list of all backups
 Get-Backup -Filter '*Console*'
 Returns a list of backups that match the given criteria
 #>
-function Get-Backup([string]$Filter, [string]$BackupPath = $Script:BACKUP_PATH) {
-    $Res = Get-ChildItem -Path $BackupPath -Exclude '__BACKUPS__.csv'
+function Get-Backup(
+    # Filter the list of backup items
+    [Alias("Name", "Item")]
+    [string] $Filter,
+
+    # Backups location
+    [ValidateScript({ Test-Path $_ })]
+    [Alias("Path", "Destination", "DestinationPath")]
+    [string]$BackupPath = $Script:BACKUP_PATH
+) {
+    $Res = Get-ChildItem -Path $BackupPath -Exclude '__BACKUPS__.csv' -Recurse
     if ($Filter) {
         $Res = $Res | Where-Object { $_.Name -Like $Filter }
     }
--- PowerShell/Modules/Backup/Functions/Restore-Item.ps1
+++ PowerShell/Modules/Backup/Functions/Restore-Item.ps1
@@ -7,37 +7,53 @@
 Restores an item from the backup
 .DESCRIPTION
 Restores the most recent copy of the given item from the defined backup folder
+.EXAMPLE
+Restore-Item README
+Restores the most recent backup of the README to the current directory
+.EXAMPLE
+Restore-Item -Name README -Path Git
+Restores the most recent backup of the README to the `Git` folder
 #>
-function Restore-Item(
-    [Parameter(Mandatory, ValueFromPipeline, ValueFromPipelineByPropertyName)]
-    [string]$Name,
+function Restore-Item {
 
-    [ValidateScript({ Test-Path $_ })]
-    [string]$Path = ($PWD.Path),
+    [CmdletBinding(SupportsShouldProcess = $True, ConfirmImpact = "HIGH")]
+    Param (
+        # The item to restore
+        [Parameter(Mandatory, ValueFromPipeline, ValueFromPipelineByPropertyName)]
+        [Alias("Item", "Input")]
+        [string]$Name,
+    
+        # The path to restore the item to
+        [ValidateScript({ Test-Path $_ })]
+        [Alias("Output", "DestinationPath")]
+        [string]$Path = ($PWD.Path),
+    
+        # Path to the backup directory
+        [ValidateScript({ Test-Path $_ })]
+        [string] $BackupPath = $Script:BACKUP_PATH,
+    
+        [ValidateSet("Archive", "Copy")]
+        [string]$Type = "Archive"
+    )
 
-    [string]$BackupPath = $Script:BACKUP_PATH,
+    # Get the most recent backup item
+    $MostRecentBackup = Get-Backup -Filter *$Name* | Select-Object -First 1
 
-    [ValidateSet("Archive", "Copy")]
-    [string]$Type = "Archive",
+    if (-Not $MostRecentBackup) { throw "Failed to find any backups with matching criteria" }
 
-    [switch] $WhatIf,
-
-    [switch] $Confirm,
-
-    # Force the operation
-    [switch] $Force
-) {
-    $MostRecentItem = Get-Backup -Filter *$Name* | Select-Object -First 1
-
-    if (-Not $MostRecentItem) { throw "Failed to find any backups with matching criteria" }
-
+    # Resolve the destination path
     $Path = Resolve-Path $Path
 
     # TODO: Select "Archive" or "Copy" based on the file extension
+
+    # Check should process
+    if (-Not ($PSCmdlet.ShouldProcess($Path, "Restoring $($MostRecentBackup.Name)"))) { return }
+
+    # Restore Item to the Destination Path
     if ($Type -eq "Archive") {
-        Expand-Archive -Path $MostRecentItem -DestinationPath $Path -Confirm:$Confirm -WhatIf:$WhatIf -Force:$Force
+        Expand-Archive -Path $MostRecentBackup -DestinationPath $Path -Force
     }
     if ($Type -eq "Copy") {
-        Copy-Item -Path $MostRecentItem -Destination "$Path\$MostRecentItem" -Confirm:$Confirm -WhatIf:$WhatIf -Force:$Force
+        Copy-Item -Path $MostRecentBackup -Destination "$Path\$MostRecentBackup" -Force
     }
 }
