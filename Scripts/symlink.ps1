1013fdd7b282a25afde9c688f7890d8705946084
Shresht Srivastav <59516096+Shresht7@users.noreply.github.com>
2022-12-07 01:23:47 +0530

    Update Symlinks Script

--- Scripts/symlink.ps1
+++ Scripts/symlink.ps1
@@ -9,69 +9,47 @@ Symlinks everything in the right place
 Symlinks dotfiles, powershell modules, and settings to their correct homes
 #>
 
-[CmdletBinding()]
-param (
-    # Shows the potential consequences of your actions. Does not actually perform them
-    [Parameter()]
-    [switch]$WhatIf = $WhatIfPreference,
+[CmdletBinding(SupportsShouldProcess)]
+param ()
 
-    # If True, will ask you to confirm before performing any modification
-    [Parameter()]
-    [switch]$Confirm
-)
-
-# Exit script if not in administrator mode
-if (-Not (Test-IsElevated)) { Write-Error -Message "Requires administrator privileges"; return }
-
-# Symlink Paths
-# -------------
-
-$Links = @(
-    @{
-        Path   = "$HOME\.gitconfig"
-        Target = "$HOME\Configs\Git\.gitconfig"
+begin {
+    <#
+    .SYNOPSIS
+    Checks if the current script has administrator privileges
+    .DESCRIPTION
+    Checks if the current script is running in administrator mode. Return true if it does, false if it doesn't
+    #>
+    function Test-IsElevated {
+        # Get the ID and security principal of the current user account
+        $MyIdentity = [System.Security.Principal.WindowsIdentity]::GetCurrent()
+        $MyPrincipal = New-Object System.Security.Principal.WindowsPrincipal($MyIdentity)
+        # Check to see if we are currently running in "Administrator" mode
+        return $MyPrincipal.IsInRole([System.Security.Principal.WindowsBuiltInRole]::Administrator)
     }
-    @{
-        Path   = "$HOME\Documents\PowerShell\Microsoft.PowerShell_profile.ps1"
-        Target = "$HOME\Configs\PowerShell\Microsoft.PowerShell_profile.ps1"
-    }
-    @{
-        Path   = "$HOME\AppData\Roaming\Code\User\settings.json"
-        Target = "$HOME\Configs\VSCode\settings.json"
-    }
-    @{
-        Path   = "$HOME\AppData\Local\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState\settings.json"
-        Target = "$HOME\Configs\Windows-Terminal\settings.json"
-    }
-)
 
-# Create symbolic links for dotfiles and settings
-foreach ($Link in $Links) {
-    New-Item -ItemType SymbolicLink -Path $Link.Path -Target $Link.Target -Force -WhatIf:$WhatIf -Confirm:$Confirm | Format-List
+
+    # Exit script if not in administrator mode
+    if (-Not (Test-IsElevated)) {
+        Write-Error -Message "Requires administrator privileges"
+        return
+    }
+    
+    # Symlink Paths
+    # -------------
+    
+    $Links = Import-Csv -Path "$PSScriptRoot\symlinks.csv"
 }
 
-# Symlink PowerShell Modules
-# --------------------------
+process {
+    # Create symbolic links for dotfiles and settings
+    foreach ($Link in $Links) {
+        if ($PSCmdlet.ShouldProcess($Link.Path)) {
+            New-Item -ItemType SymbolicLink -Path $Link.Path -Target $Link.Target -Force | Format-List
+        }
+    }
 
-# Create symbolic links for powershell modules
-Get-ChildItem "$HOME\Configs\PowerShell\Modules" | ForEach-Object {
-    New-Item -ItemType SymbolicLink -Path "$HOME\Documents\PowerShell\Modules\$($_.Name)" -Target "$HOME\Configs\PowerShell\Modules\$($_.Name)" -Force -WhatIf:$WhatIf -Confirm:$Confirm | Format-List
 }
 
-# -------
-# Helpers
-# -------
+end {
 
-<#
-.SYNOPSIS
-Checks if the current script has administrator privileges
-.DESCRIPTION
-Checks if the current script is running in administrator mode. Return true if it does, false if it doesn't
-#>
-function Test-IsElevated {
-    # Get the ID and security principal of the current user account
-    $MyIdentity = [System.Security.Principal.WindowsIdentity]::GetCurrent()
-    $MyPrincipal = New-Object System.Security.Principal.WindowsPrincipal($MyIdentity)
-    # Check to see if we are currently running in "Administrator" mode
-    return $MyPrincipal.IsInRole([System.Security.Principal.WindowsBuiltInRole]::Administrator)
 }
