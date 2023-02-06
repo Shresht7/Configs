f4a7430eef38443c04c6a841151e5705d1aa1e17
Shresht Srivastav <59516096+Shresht7@users.noreply.github.com>
2023-02-06 16:18:06 +0530

    Move `AutoComplete` setup back to the profile

--- PowerShell/Microsoft.PowerShell_profile.ps1
+++ PowerShell/Microsoft.PowerShell_profile.ps1
@@ -214,11 +214,41 @@ Import-Module Terminal-Icons
 
 Import-Module z
 
+# =============
+# Auto-Complete
+# =============
+
+# GitHub CLI
+if (Find-Path -Command gh) {
+    Invoke-Expression -Command $(gh completion -s powershell | Out-String)
+}
+
+# Deno
+if (Find-Path -Command deno) {
+    Invoke-Expression -Command $(deno completions powershell | Out-String)
+}
+
+# WinGet
+# https://github.com/microsoft/winget-cli/blob/master/doc/Completion.md
+Register-ArgumentCompleter -Native -CommandName winget -ScriptBlock {
+    param($wordToComplete, $commandAst, $cursorPosition)
+    [Console]::InputEncoding = [Console]::OutputEncoding = $OutputEncoding = [System.Text.Utf8Encoding]::new()
+    $Local:word = $wordToComplete.Replace('"', '""')
+    $Local:ast = $commandAst.ToString().Replace('"', '""')
+    winget complete --word="$Local:word" --commandline "$Local:ast" --position $cursorPosition | ForEach-Object {
+        [System.Management.Automation.CompletionResult]::new($_, $_, 'ParameterValue', $_)
+    }
+}
+
+
 # =======
 # PSStyle
 # =======
 
+# ---------------------------------------------
 $PSAccentColor = $PSStyle.Foreground.BrightCyan
+# ---------------------------------------------
 
+# Use the accent color
 $PSStyle.Formatting.TableHeader = $PSAccentColor
 $PSStyle.Formatting.FormatAccent = $PSAccentColor
