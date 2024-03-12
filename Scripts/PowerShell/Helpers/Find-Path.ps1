e79150adc32d20f1457483fed54d0ad623dede31
Shresht Srivastav <59516096+Shresht7@users.noreply.github.com>
2024-03-12 18:41:43 +0530

    Update docstrings

--- Scripts/PowerShell/Helpers/Find-Path.ps1
+++ Scripts/PowerShell/Helpers/Find-Path.ps1
@@ -1,14 +1,17 @@
 <#
 .SYNOPSIS
-Find the path of the given program's executable
+    Find the path of the given program's executable
 .DESCRIPTION
-Locates the path for the given program's executable like the Unix `which` command.
-.PARAMETER $Command
-Name of the command
+    Locates the path for the given program's executable like the Unix `which` command.
 .EXAMPLE
-Find-Path git
-Returns C:\Program Files\Git\cmd\git.exe
+    Find-Path git
+    Returns C:\Program Files\Git\cmd\git.exe
 #>
-function Find-Path([string]$Command) {
+function Find-Path(
+    # Name of the command to lookup
+    [Parameter(Mandatory, ValueFromPipeline, ValueFromPipelineByPropertyName)]
+    [Alias("Name", "Program", "Executable")]
+    [string] $Command
+) {
     Get-Command -Name $Command -ErrorAction SilentlyContinue | Select-Object -ExpandProperty Path -ErrorAction SilentlyContinue
 }
