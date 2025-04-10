a0a55259502baa26b6d8d4f58e4a5975d2756e35
Shresht Srivastav <59516096+Shresht7@users.noreply.github.com>
2025-04-10 19:41:46 +0530

    Replace notify-send with zenity for better user interaction and add error handling to the script

--- GNOME/nautilus/scripts/hello_world.sh
+++ GNOME/nautilus/scripts/hello_world.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
-notify-send $NAUTILUS_SCRIPT_SELECTED_FILE_PATHS
+# Exit the script on any errors as this will run in the background, we don't want it to be stuck and taking up memory
+set -e  # Causes the script to exit im immediately if there are any errors.
+set -u  # Causes the script to exit if we do not define a variable when it is expanded.
+set -o pipefail # Makes sure that if any part of a pipeline fails, the script knows about.
+
+zenity --info --title "Selected Files" --text "$NAUTILUS_SCRIPT_SELECTED_FILE_PATHS"
