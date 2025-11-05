c96fcbe3489d42b68c009d01582a2e44cfa6c3b8
Shresht Srivastav <59516096+Shresht7@users.noreply.github.com>
2025-11-05 10:17:18 +0530

    Add section for other modules

--- PowerShell/Microsoft.PowerShell_profile.ps1
+++ PowerShell/Microsoft.PowerShell_profile.ps1
@@ -281,3 +281,10 @@ $PSAccentColor = $PSStyle.Foreground.BrightCyan
 # Use the accent color
 $PSStyle.Formatting.TableHeader = $PSAccentColor
 $PSStyle.Formatting.FormatAccent = $PSAccentColor
+
+# OTHER MODULES
+# -------------
+
+#f45873b3-b655-43a6-b217-97c00aa0db58 PowerToys CommandNotFound module
+Import-Module -Name Microsoft.WinGet.CommandNotFound
+Import-Module -Name Microsoft.PowerShell.PSAdapter
