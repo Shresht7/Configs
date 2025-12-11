6ead99b8b79512a45d0f8b92b7eb9ac05e2d453b
Shresht Srivastav <59516096+Shresht7@users.noreply.github.com>
2025-12-11 10:27:56 +0530

    Add conditional imports for Windows-specific modules in PowerShell profile

--- PowerShell/Microsoft.PowerShell_profile.ps1
+++ PowerShell/Microsoft.PowerShell_profile.ps1
@@ -292,5 +292,7 @@ $PSStyle.Formatting.FormatAccent = $PSAccentColor
 # -------------
 
 #f45873b3-b655-43a6-b217-97c00aa0db58 PowerToys CommandNotFound module
-Import-Module -Name Microsoft.WinGet.CommandNotFound
-Import-Module -Name Microsoft.PowerShell.PSAdapter
+if ($IsWindows) {
+    Import-Module -Name Microsoft.WinGet.CommandNotFound
+    Import-Module -Name Microsoft.PowerShell.PSAdapter    
+}
